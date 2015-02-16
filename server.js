54aedbc9254fb5c899cb3a1005094b4ff04a7252
lilymaster@gmail.com <lilymaster@gmail.com>
2015-02-16 22:29:10 +0200

    added express for node as server

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(request, response) {
+app.use(express.static(__dirname)); // app.use(express.static(__dirname + '/public'));
+
+
+/*app.get('/', function(request, response) {
 response.send('Hello World!');
-});
+});*/
+
 app.listen(app.get('port'), function() {
 console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+/*var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function() {
+console.log("Node app is running at localhost:" + app.get('port'));
+});*/
